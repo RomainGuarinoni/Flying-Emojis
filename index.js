3898e4bc9b2852279a8cf13dd3260b56ec8a3f97
romainGuarinoni <romain.guar91@gmail.com>
2021-08-13 23:06:23 +0200

    feat(core): add chaos mode

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let settingsOpen = false;
-
+let chaos = false;
 //variable for settings options
 let translateX = false;
 let rotate = false;
@@ -7,6 +7,7 @@ let scale = 1;
 let duration = 1000;
 
 const settingsButton = document.getElementById("settingWheel");
+const chaosButton = document.getElementById("chaos");
 const settingsWindow = document.getElementById("settingsContainer");
 const translateXButton = document.getElementById("input-translateX");
 const rotateButton = document.getElementById("input-rotate");
@@ -73,7 +74,13 @@ for (let index = 0; index < emojis.length; index++) {
  * @param {HTMLElement} icon
  * @return {void} add a flying emojy to the HTML body
  */
-function createFlyingEmoji(icon) {
+function createFlyingEmoji(
+  icon,
+  translateXChaos = false,
+  rotateChaos = false,
+  scaleChaos = undefined,
+  durationChaos = undefined
+) {
   const positionX = Math.random() * window.innerWidth;
   const bezier = `cubic-bezier(${Math.random()},${Math.random()},${Math.random()},${Math.random()})`;
   //HTML property
@@ -84,14 +91,23 @@ function createFlyingEmoji(icon) {
   flyingEmoji.style.position = "absolute";
   flyingEmoji.style.left = `${positionX}px`;
   flyingEmoji.style.bottom = `-5px`;
-  const keyFramesName = createRandomAnimation(duration);
-  flyingEmoji.style.animation = `${keyFramesName} ${duration}ms ${bezier}`;
+  const keyFramesName = createRandomAnimation(
+    translateXChaos,
+    rotateChaos,
+    scaleChaos
+  );
+  flyingEmoji.style.animation = `${keyFramesName} ${
+    durationChaos ? durationChaos : duration
+  }ms ${bezier}`;
   document.body.appendChild(flyingEmoji);
 
   //remove the node when animation is done
-  setTimeout(() => {
-    document.body.removeChild(flyingEmoji);
-  }, duration);
+  setTimeout(
+    () => {
+      document.body.removeChild(flyingEmoji);
+    },
+    durationChaos ? durationChaos : duration
+  );
 }
 
 /**
@@ -109,7 +125,7 @@ function createUUID() {
 /**
  * @return {string} the uuid Name of the animation
  */
-function createRandomAnimation() {
+function createRandomAnimation(translateXChaos, rotateChaos, scaleChaos) {
   const css = window.document.styleSheets[0];
   const keyFramesName = "keyframe-" + createUUID();
   const index = css.cssRules.length;
@@ -121,18 +137,50 @@ function createRandomAnimation() {
         }
         100% {
           transform: translate(${
-            translateX
+            translateXChaos || translateX
               ? Math.random() > 0.5
                 ? Math.random() * document.body.clientWidth
                 : -Math.random() * document.body.clientWidth
               : 0
           }px,-${document.body.clientHeight + 50}px) rotate(${
-      rotate ? "180deg" : 0
-    }) scale(${scale}) ;
+      rotate || rotateChaos ? "180deg" : 0
+    }) scale(${scaleChaos ? scaleChaos : scale}) ;
           display:none;
         }
       }`,
     index
   );
   return keyFramesName;
+}
+
+chaosButton.addEventListener("click", () => {
+  chaos = !chaos;
+  // const interval;
+  if (chaos) {
+    chaosButton.style.color = "#f44336";
+    createChaos();
+  } else {
+    // interval.clearInterval()
+    chaosButton.style.color = "rgba(0, 0, 0, 0.425)";
+  }
+});
+
+/**
+ * @description All the emojis are flying in full chaos
+ */
+function createChaos() {
+  const emojisIcon = [];
+  for (let i = 0; i < emojis.length; i++) {
+    emojisIcon.push(emojis[i].textContent);
+  }
+  setInterval(() => {
+    const randomIndex = Math.floor(Math.random() * emojisIcon.length);
+    createFlyingEmoji(
+      emojisIcon[randomIndex],
+      true,
+      true,
+      Math.random() * 20,
+      Math.random() * 2000
+    );
+  }, 1);
 }
